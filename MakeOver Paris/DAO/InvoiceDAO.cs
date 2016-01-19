107157988b4fd283c9360a664c7a6f82cd9d2771
Menghok <menghok.heak@gmail.com>
2016-01-19 23:49:51 +0700

    Update DTO changing Namespace

---
+++ MakeOver Paris/DAO/InvoiceDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using MakeOver_Paris.DTO;
+
+namespace MakeOver_Paris.DAO
+{
+    class InvoiceDAO
+    {
+
+        public Boolean addInvoice()
+        {
+            
+            return false;
+        }
+
+    }
+}
--- MakeOver Paris/DTO/Member.cs
+++ MakeOver Paris/DTO/Member.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace POS.DTO
+namespace MakeOver_Paris.DTO
 {
     class Member
     {
