d1c40495e06d9cff4053a1a2bbd69f7bcdf50873
Menghok <menghok.heak@gmail.com>
2016-02-13 12:13:28 +0700

    For storing login information

---
+++ MakeOver Paris/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeOver_Paris
+{
+    class Data
+    {
+        public static DTO.Staff user;
+    }
+}
